8e728a3e1d74f06327b93ccd035a9c7b57148c91
Leandro Souza <leandro.alcantara.souza@gmail.com>
2019-04-22 20:49:52 -0300

    Configuring proxy on development

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  componentDidMount() {
+
+    axios.get('/api/v1/todos?contentFilter=blah&page=0&size=10')
+      .then(function (response) {
+        // handle success
+        console.log(response);
+      })
+  }
+
   render() {
     return (
       <div className="App">
