ba1083b8db8a6844b391b9af378d062ee707d79f
Leandro Souza <leandro.alcantara.souza@gmail.com>
2019-04-23 23:00:48 -0300

    Finishing modal component with disabled behaviour if empty field

--- src/new-todo/NewTodo.js
+++ src/new-todo/NewTodo.js
@@ -17,26 +17,43 @@ class NewTodo extends Component {
     this.onchangeTodo = this.onchangeTodo.bind(this);
     this.todoDto = new TodoDTO();
     this.state = {todo: this.todoDto};
+    this.disabled = true;
   };
-
 
   createTodo(event) {
     event.preventDefault();
     let serviceTodo = new ServiceTodo();
     serviceTodo.createTodo(this.state.todo).then((response) => console.log(response));
     this.back();
-  }
+  };
 
   onchangeTodo(event) {
     this.todoDto.content = event.target.value;
+    let contentTrimmed = this.todoDto.content && this.todoDto.content.trim();
+    if(contentTrimmed !== "") {
+      this.disabled = false;
+    }
+    else {
+      this.disabled = true;
+    }
     this.setState({todo: this.todoDto})
-  }
+  };
 
   back = (e) => {
     if(e) {
       e.preventDefault();
     }
     this.history.goBack();
+  };
+
+  disableSubmitOnEmptyForm = (event) => {
+    if(event) {
+      let pressedValue = event.charCode || event.keyCode;
+      if(pressedValue === 13 && this.state.todo.content.trim() === "") {
+        event.preventDefault();
+        return false;
+      }
+    }
   };
 
   render() {
@@ -46,7 +63,7 @@ class NewTodo extends Component {
           <Modal.Header className={"bg-info"}>
             <Modal.Title className={"font-weight-light text-capitalize"} style={{color:"#fff"}}>New Todo</Modal.Title>
           </Modal.Header>
-          <Form onSubmit={this.createTodo}>
+          <Form onKeyPress={this.disableSubmitOnEmptyForm} >
             <Modal.Body>
                 <Row>
                   <Col>
@@ -57,7 +74,9 @@ class NewTodo extends Component {
             <Modal.Footer style={{justifyContent: "flex-start"}}>
               <Row>
                 <Col>
-                    <button type={"submit"} className={"btn"}><i className={"far fa-thumbs-up"} style={{fontSize: "2em", color: "green"}}/></button>
+                  <button onClick={this.createTodo} disabled={this.disabled} type={"submit"} className={"btn"}>
+                    <i className={"far fa-thumbs-up"} style={{fontSize: "2em", color: "green"}}/>
+                  </button>
                 </Col>
                 <Col >
                   <button onClick={this.back} className={"btn"}><i className={"far fa-thumbs-down"} style={{fontSize: "2em", color: "red"}}/></button>
